9ccce2ec678bb70e797939d2f1923ff71862a37c
tucamoto <86570346+tucamoto@users.noreply.github.com>
2023-08-31 11:36:45 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+const express    = require('express');
+const exphbs     = require('express-handlebars');
+const app        = express();
+const path       = require('path');
+const db         = require('./db/connection');
+const bodyParser = require('body-parser');
+const Job        = require('./models/Job');
+const Sequelize  = require('sequelize');
+const Op         = Sequelize.Op;
+
+const PORT = 3000;
+
+app.listen(PORT, function() {
+  console.log(`Express is running on the port ${PORT}`);
+});
+
+// body-parser
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// handle-bars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs.create({defaultLayout: 'main'}).engine);
+app.set('view engine', 'handlebars');
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// db connection
+db
+  .authenticate()
+  .then(() => {
+    console.log("Connected to the bank successfully");
+  })
+  .catch(err => {
+    console.log("There was an error connecting", err);
+  });
+
+// routes
+app.get('/', (req, res) => {
+
+  let search = req.query.job;
+  let query  = '%'+search+'%'; // PH -> PHP, Word -> Wordpress, press -> Wordpress
+
+  if(!search) {
+    Job.findAll({order: [
+      ['createdAt', 'DESC']
+    ]})
+    .then(jobs => {
+  
+      res.render('index', {
+        jobs
+      });
+  
+    })
+    .catch(err => console.log(err));
+  } else {
+    Job.findAll({
+      where: {title: {[Op.like]: query}},
+      order: [
+        ['createdAt', 'DESC']
+    ]})
+    .then(jobs => {
+      console.log(search);
+      console.log(search);
+  
+      res.render('index', {
+        jobs, search
+      });
+  
+    })
+    .catch(err => console.log(err));
+  }
+
+  
+});
+
+// jobs routes
+app.use('/jobs', require('./routes/jobs'));
